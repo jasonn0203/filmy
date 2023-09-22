1733a9e59a0a8a45a514927c60141bcfe53e41e5
Jason Huynh <jasonn0203@gmail.com>
2023-09-22 23:39:40 +0700

    đổi font

--- src/app/components/cast/cast-detail/cast-detail.component.ts
+++ src/app/components/cast/cast-detail/cast-detail.component.ts
@@ -28,4 +28,27 @@ export class CastDetailComponent implements OnInit {
       this.castDetailResult = data;
     });
   }
+
+  calculateAge(birthday: string): number | null {
+    const birthDate = new Date(birthday);
+    const currentDate = new Date();
+
+    // Check if the provided date is valid
+    if (isNaN(birthDate.getTime())) {
+      return null;
+    }
+
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    // Adjust age if the birthday has not occurred this year yet
+    if (
+      currentDate.getMonth() < birthDate.getMonth() ||
+      (currentDate.getMonth() === birthDate.getMonth() &&
+        currentDate.getDate() < birthDate.getDate())
+    ) {
+      return age - 1;
+    }
+
+    return age;
+  }
 }
